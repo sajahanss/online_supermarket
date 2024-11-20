a0885ee6d81a7980eb338b918a026769eb449df0
sajahanss <sajahanshitthu@gmail.com>
2024-11-21 01:15:29 +0530

    first commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -16,11 +16,11 @@ app.use(express.json())
 app.use(express.json({limit: '150mb'}));
 app.use(express.urlencoded({limit: '150mb'}));
 
-router.route('/').get(getProducts).post(protect, admin, createProduct)
-router.route('/:id/reviews').post(protect, createProductReview)
-router.get('/top', getTopProducts)
+router.route('/' || '/api/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews' || '/api/:id/reviews').post(protect, createProductReview)
+router.get('/top' || '/api/top', getTopProducts)
 router
-  .route('/:id')
+  .route('/:id' || '/api/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct)
